7e35d73153b1872d745920ce4583fa2a31b7e19c
ZachyDev <mosetizachary001@gmail.com>
2020-06-04 01:13:46 +0300

    eldo js member registration

---
+++ register/js/firebase_config.js
@@ -0,0 +1,24 @@
+// web configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA7AvBmwVeg3WHkDmhU8quVhaabD-ZiqmI",
+    authDomain: "eldoretjs-register.firebaseapp.com",
+    databaseURL: "https://eldoretjs-register.firebaseio.com",
+    projectId: "eldoretjs-register",
+    storageBucket: "eldoretjs-register.appspot.com",
+    messagingSenderId: "304066260240",
+    appId: "1:304066260240:web:bd517dc6e509fe4b17c995"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+//   create a reference to register collection
+const registerRef = firebase.database().ref('register');
+
+// listen to submit event
+document.getElementById('submitUser').addEventListener('submit',registerUser);
+
+// registerUser function
+function registerUser(e){
+    e.preventDefault();
+    alert('hello')
+}
